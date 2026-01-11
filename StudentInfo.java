9256904b4698bc70c963cd71983d08d46505f61f
akshayaasa81-del <akshayaasa81@gmail.com>
2026-01-11 18:43:33 +0530

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,12 @@
+package com.example.studentinfo;
+
+import java.util.*;
+
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Student Name: Akshayaa S A");
+        System.out.println("Register Number: 23MIC0016");
+        System.out.println("Message: Jenkins–GitHub Integration Successful");
+    }
+}
